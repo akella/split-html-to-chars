6e532fe319f4a5bf6237837c6bc7b2530cddf764
akella <akella.a@gmail.com>
2017-06-24 01:57:04 +0300

    update package

--- rollup.config.js
+++ rollup.config.js
@@ -12,9 +12,6 @@ export default {
 		}),
         commonjs({
           namedExports: {
-            // left-hand side can be an absolute path, a path
-            // relative to the current directory, or the name
-            // of a module in node_modules
             'node_modules/html-parse-stringify/index.js': [ 'HTML' ]
           }
         })
